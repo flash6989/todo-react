675e9cc898583eec9638fc095de25ab1a15a8fab
Flash6989 <flash6989@yandex.ru>
2022-01-12 19:06:34 +0400

    Updates

---
+++ precache-manifest.ba143227bb57a6a76c12a34a27582aa4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "514ccb4979fa573161946c2a810cc7f5",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "69d99d3f8dd45e95f4b5",
+    "url": "/todo-react/static/css/main.f397faf5.chunk.css"
+  },
+  {
+    "revision": "899712c8debbce5cb876",
+    "url": "/todo-react/static/js/2.a277081d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-react/static/js/2.a277081d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69d99d3f8dd45e95f4b5",
+    "url": "/todo-react/static/js/main.956959ee.chunk.js"
+  },
+  {
+    "revision": "1614377f578425b5d648",
+    "url": "/todo-react/static/js/runtime-main.92cd31f8.js"
+  }
+]);
